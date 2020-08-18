99396a118d1114652c66e83ee43ba864e23d1daf
matinavarro <mnla12@hotmail.com>
2020-08-17 21:29:13 -0300

    Trabajo final

---
+++ Auto2.h
@@ -0,0 +1,22 @@
+#ifndef AUTO2_H
+#define AUTO2_H
+#include "Auto.h"
+#include <ctime>
+
+class Auto2:public Auto {
+protected:
+	bool cambio;
+	int colorAuto;
+
+public:
+	Auto2(int v, int x0,int y0);
+	Auto2(){};
+	void Cambio();
+	void update();
+	
+	
+	
+};
+
+
+#endif
---
+++ Auto3.h
@@ -0,0 +1,24 @@
+#ifndef AUTO3_H
+#define AUTO3_H
+#include "Auto.h"
+#include <ctime>
+
+class Auto3:public Auto{
+protected:
+	bool cambio;
+	int colorAuto;
+	
+	
+public:
+
+	Auto3(int v, int x0,int y0);
+	Auto3(){};
+	void Colorear();
+	void update();
+	
+	
+	
+};
+
+
+#endif
